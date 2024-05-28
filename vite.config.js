3ab641e8c294c4f6a92e1b249a32eb85414ccffa
hurjw98 <hurjw98@gmail.com>
2024-05-28 21:20:46 +0900

    npm run build를 실행

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-team-score/',  // GitHub repository 이름으로 설정
+  base: '/react-team-score/',  // 리포지토리 이름으로 설정
 });
+
