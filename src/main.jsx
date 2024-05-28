3165d89fbcf389fceab124f2476bd6204d6e4f9c
hurjw98 <hurjw98@gmail.com>
2024-05-28 13:47:09 +0900

    실행이 되지 않는 문제 해결 (env 삭제 등)

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
@@ -9,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <HashRouter basename={import.meta.env.VITE_PUBLIC_URL}>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
