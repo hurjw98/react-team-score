8d8cb575866abbb817b5569d7e081623576b8210
hurjw98 <hurjw98@gmail.com>
2024-05-28 09:37:45 +0900

    다시 HashRouter로 바꾸고 ScrollToHashElement를 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 // App.jsx
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+} from "react-router-dom";
+import { useEffect } from "react";
 import "./App.css";
 
 import MainHeader from "./components/MainHeader";
@@ -7,9 +13,30 @@ import FirstPage from "./pages/FirstPage";
 import NotFound from "./pages/NotFound";
 import TeamScore from "./pages/TeamScore";
 
+const ScrollToHashElement = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    const unlisten = history.listen((location) => {
+      const targetElement = document.getElementById(
+        decodeURIComponent(location.hash.slice(1))
+      );
+
+      if (targetElement) {
+        window.scrollTo({ top: targetElement.offsetTop, behavior: "smooth" });
+      }
+    });
+
+    return () => unlisten();
+  }, [history]);
+
+  return null;
+};
+
 function App() {
   return (
     <Router basename={import.meta.env.VITE_PUBLIC_URL}>
+      <ScrollToHashElement />
       <MainHeader />
       <main>
         <Switch>
