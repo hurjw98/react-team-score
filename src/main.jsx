1f5824ce7d45173c2a05d2365607cdce26644fd3
hurjw98 <hurjw98@gmail.com>
2024-05-28 22:09:56 +0900

    BrowserRouter의 basename 변경 해보기

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-team-score">
+    <BrowserRouter basename={"/react-team-score"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
